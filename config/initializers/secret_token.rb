22411799753bdf41d56433b7d7c32680e8709cfa
Clemens Park <clemens.park@gmail.com>
2013-06-14 09:46:30 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AssetTracker::Application.config.secret_token = '4c3a36ccc2024e71c2dc19c7d8313eb91c86ed541225cbb3fce4ee9e4ea834d55f74ff6015c4321ed41564ff6edca44ac225030e7b6878dc3811653a2a930b1a'
